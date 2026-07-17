31e4a868bcb9085c753f3c8ed89d92cfb9eb0428
Ethen Wang <ethanwang@Ethens-MacBook-Air.local>
2026-07-16 23:59:45 -0400

    Added windows XP background, middle section of an image, and added more details and polished coding variables and debugs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-document.querySelector("#app").innerHTML = "<p>loading...</p>";
-
 fetch(
-  "https://api.open-meteo.com/v1/forecast?latitude=43.6532&longitude=-79.3832&current=temperature_2m,weather_code,is_day",
+  `https://api.open-meteo.com/v1/forecast?latitude=43.6532&longitude=-79.3832&current=temperature_2m,weather_code,is_day`,
 )
   .then((response) => response.json())
   .then((data) => {
@@ -10,20 +8,31 @@ fetch(
     const code = current.weather_code;
     const isDay = current.is_day;
 
-    document.querySelector("#app").innerHTML = `
-            <h1>${temp}°C</h1>
-            <p>${getWeatherDescription(code, isDay)}</p>
-            `;
+    const description = getWeatherDescription(code, isDay);
+
+    document.querySelector("#weather").innerHTML = `
+      <h1>${temp}°C</h1>
+      <p>${description}</p>
+    `;
   })
   .catch((err) => {
-    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+    document.querySelector("#weather").innerHTML =
+      `<p>Error: ${err.message}</p>`;
   });
 
+document.querySelector("#searchBox").addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const query = e.target.value;
+    window.location.href = `https://www.google.com/search?q=${query}`;
+  }
+});
+
 function getWeatherDescription(code, isDay) {
-  if (code === 0) return isDay ? "Clear sky!" : "Clear night.";
-  if (code <= 3) return "Cloudy...";
-  if (code === 45 || code === 48) return "Foggy...";
-  if (code >= 51 && code <= 67) return "Rainy.";
-  if (code >= 95) return "Thunderstorm!!";
-  return "Unknown?!";
+  if (code === 0) return isDay ? "Clear sky" : "Clear night";
+  if (code <= 3) return "Cloudy";
+  if (code === 45 || code === 48) return "Foggy";
+  if (code >= 51 && code <= 67) return "Rainy";
+  if (code >= 71 && code <= 86) return "Snowing";
+  if (code >= 95) return "Thunderstorm";
+  return "Unknown";
 }
